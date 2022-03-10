f985a978f868e21a5420123e8f3fd9b73e1c5244
Dirk Knibbe <dirkknibbe@Dirks-MacBook-Pro.local>
2022-03-10 12:25:54 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const PORT = process.env.PORT || 9000;
+
+const express = require("express");
+const server = express();
+
+server.get("/", (req, res) => {
+  res.end("<h1>hello world<h1>");
+});
+
+server.listen(PORT, () => {
+  console.log("here is my qwerty variable: ", process.env.QWERTY);
+  console.log(
+    `server is running on port ${PORT}, user is ${process.env.USER}!`
+  );
+});
